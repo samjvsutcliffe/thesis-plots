593a9f820fad3f08e5be9fb432a2843b8d0f4165
Sam <sam.sutcliffe2@gmail.com>
2026-05-26 16:13:03 +0100

    Added column example

---
+++ methods/dr/plot_all.py
@@ -0,0 +1,100 @@
+import matplotlib as mpl
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib.patches import Rectangle
+from matplotlib.collections import PatchCollection
+from matplotlib import cm
+import re
+import os
+import json
+import numpy as np
+import pandas as pd
+import json
+import sys
+from vtk import vtkUnstructuredGridReader
+from vtk.util import numpy_support as VN
+from vtk.util.numpy_support import vtk_to_numpy, numpy_to_vtk
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib.patches import Rectangle
+from matplotlib.collections import PatchCollection
+from matplotlib import cm
+from multiprocessing import Pool
+
+plt.style.use("seaborn-paper")
+plt.rc('font', family='serif', serif='Times')
+# plt.rc('text', usetex=True)
+plt.rc('xtick', labelsize=8)
+plt.rc('ytick', labelsize=8)
+plt.rc('axes', labelsize=8)
+width = 3.487
+height = width / 1.618
+scale = 1
+fig = plt.figure(figsize=(scale*width,1.3*scale*height),dpi=200)
+
+
+top_dir = "./data/"
+output_regex = re.compile(".*")
+output_list = list(filter(lambda x: not os.path.isfile(os.path.join(top_dir,x)),filter(output_regex.match,os.listdir(top_dir))))
+output_list.sort()
+for i,out in enumerate(output_list):
+    print("{}: {}".format(i,out))
+
+marker_styles = ["1","2","3","4","x"]
+for data_tdir,ms in zip(output_list,marker_styles):
+    data_regex = re.compile("data-*")
+    data_list = list(filter(data_regex.match,os.listdir(top_dir+data_tdir)))
+    data_list.sort()
+    data_h = []
+    data_e = []
+    for f in data_list:
+        L = 50
+        rho =80
+        g = 10
+        output_dir = "{}/{}".format(top_dir+data_tdir,f)
+        x,y = f.split("-")[-1].split(".")[0].split("_")
+        refine = int(x)
+        mps = int(y)
+        df = pd.read_csv(output_dir)
+        y = df["Y"].values
+        syy = df["SYY"].values
+        syy_ref = df["SYY-REF"].values
+        vp = df["VP"].values
+        #syy-syy_ref
+        h = L / 2**refine
+        error = np.sum(np.abs(syy-syy_ref)*vp)/(rho*g*L*np.sum(vp))
+        # np.linalg.norm(syy-syy_ref)*vp[0]/(rho*g*L*np.sum(vp)))
+        data_h.append(h)
+        data_e.append(error)
+    data_h = np.array(data_h)
+    data_e = np.array(data_e)
+    plt.scatter(1/data_h,data_e,label=data_tdir,marker=ms)
+plt.xlabel("1/h")
+plt.ylabel("Normalised error")
+plt.xscale("log")
+plt.yscale("log")
+
+def plot_tri(offset,size):
+    xoffset = 10**offset[0]
+    yoffset = 10**offset[1]
+    xsize = 1+size[0]
+    ysize = 1+size[1]
+    x = [xoffset*xsize,xoffset*xsize ,xoffset]
+    y = [yoffset      ,yoffset/ysize ,yoffset]
+    pos = np.vstack((x,y)).transpose()
+    print(pos)
+    t1 = plt.Polygon(pos,color="black",fill=False,lw=1)
+    plt.gca().add_patch(t1)
+    plt.text((xoffset*xsize)*1.1,yoffset/(ysize**0.6),size[1],size="x-small")
+    # plt.plot(x,y)
+
+plot_tri([0.5,-3.5],[1,1])
+plot_tri([-0.5,-2],[1,2])
+
+
+plt.legend()
+plt.tight_layout()
+plt.savefig("conv.pdf")
+plt.show()
